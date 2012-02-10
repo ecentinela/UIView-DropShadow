fa2bcaa5c54f3fced5e93aa72d8f0f206b48ca14
Javier Martinez Fernandez <ecentinela@gmail.com>
2012-02-10 10:07:55 +0100

    Initial commit.

---
+++ UIView+DropShadow.h
@@ -0,0 +1,6 @@
+@interface UIView(DropShadow)
+
+// set the drop shadow effect
+- (void)dropShadowWithOffset:(CGSize)offset Radius:(CGFloat)radius Color:(UIColor *)color Opacity:(CGFloat)opacity;
+
+@end
---
+++ UIView+DropShadow.m
@@ -0,0 +1,24 @@
+#import "UIView+DropShadow.h"
+
+@implementation UIView(DropShadow)
+
+- (void)dropShadowWithOffset:(CGSize)offset Radius:(CGFloat)radius Color:(UIColor *)color  Opacity:(CGFloat)opacity
+{
+    // set properties
+    self.layer.shadowColor = color.CGColor;
+    self.layer.shadowOffset = offset;
+    self.layer.shadowRadius = radius;
+    self.layer.shadowOpacity = opacity;
+
+    // disable clipsToBounds to not clip off the shadow
+    self.clipsToBounds = NO;
+
+    // better performance
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, self.bounds);
+    self.layer.shadowPath = path;
+    CGPathCloseSubpath(path);
+    CGPathRelease(path);
+}
+
+@end
